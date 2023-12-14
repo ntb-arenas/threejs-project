76390af4465cfdc79e0a61760c2239d9c1405c28
Niño Arenas <nino.arenas@wundermanthompson.com>
2023-12-14 17:41:16 +0000

    Update canvas shirt

---
+++ client/src/canvas/Shirt.jsx
@@ -0,0 +1,18 @@
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+import { useFrame } from "@react-three/fiber";
+import { Decal, useGLTF, useText, useTexture } from "@react-three/drei";
+
+import state from "../store";
+
+const Shirt = () => {
+  const snap = useSnapshot(state);
+  const { nodes, materials } = useGLTF("/shirt_baked.glb");
+
+  const logoTexture = useTexture(snap.logoDecal);
+  const fullTexture = useTexture(snap.fullDecal);
+
+  return <>Shirt</>;
+};
+
+export default Shirt;
