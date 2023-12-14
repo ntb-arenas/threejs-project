2088fae454b5ac6061775bffd087800cc0e39267
Niño Arenas <ntb.arenas@protonmail.com>
2023-12-14 12:47:22 +0000

    Update folder structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,15 @@
+import Canvas from "./canvas";
+import Customizer from "./pages/customizer";
+import Home from "./pages/home";
+
 function App() {
-  return <div className="head-text">React App</div>;
+  return (
+    <main className="transition-all ease-in app">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  );
 }
 
 export default App;
