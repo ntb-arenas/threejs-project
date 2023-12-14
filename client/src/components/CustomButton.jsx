d407040c961e2d40f4f5db5a5a8c8af371ae9166
Niño Arenas <ntb.arenas@protonmail.com>
2023-12-14 13:46:41 +0000

    Update custom button component

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,23 @@
+import { useSnapshot } from "valtio";
+import state from "../store";
+
+const CustomButton = ({ title, type, customStyles, handleClick }) => {
+  const snap = useSnapshot(state);
+
+  const generateStyle = (type) => {
+    if (type === "filled") {
+      return { backgroundColor: snap.color, color: "#FFF" };
+    }
+  };
+
+  return (
+    <button
+      style={generateStyle(type)}
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+    >
+      {title}
+    </button>
+  );
+};
+
+export default CustomButton;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useSnapshot } from "valtio";
 
 import state from "../store";
+import { CustomButton } from "../components";
 import {
   headContainerAnimation,
   headContentAnimation,
@@ -38,11 +39,22 @@ const Home = () => {
               </h1>
             </motion.div>
 
-            <motion.div {...headTextAnimation}>
-              <p>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md text-base font-normal text-gray-600">
                 Create your unique and exclusive shirt with our brand-new 3D
                 customization tool. <strong>Unleash your imagination</strong>{" "}
+                and define your own style.
               </p>
+
+              <CustomButton
+                type="filled"
+                title="Customize It"
+                handleClick={() => (state.intro = false)}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
             </motion.div>
           </motion.div>
         </motion.section>
