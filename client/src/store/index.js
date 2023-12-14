6cb9bf60576fdde0e2e629cc8d4273726ad0fd63
Niño Arenas <nino.arenas@wundermanthompson.com>
2023-12-14 21:03:30 +0000

    update mia logo

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,7 +5,7 @@ const state = proxy({
   color: "#EFBD48",
   isLogoTexture: true,
   isFullTexture: false,
-  logoDecal: "./threejs.png",
+  logoDecal: "./mia.png",
   fullDecal: "./threejs.png",
 });
 
