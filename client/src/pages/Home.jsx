5d50b4552b63f029d76ca6323d59169d40b792cf
Niño Arenas <ntb.arenas@protonmail.com>
2023-12-14 12:59:44 +0000

    Import Valtio

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { motion, AnimatePresence } from "framer-motion";
+import { useSnaphot } from "valtio";
+
+import state from "../store";
+import {
+  headContainerAnimation,
+  headtContentAnimation,
+  headTextAnimation,
+  slideAnimation,
+} from "../config/motion";
+
 const Home = () => {
-  return <div>Home</div>;
+  const snap = useSnapshot(state);
+
+  return <AnimatePresence></AnimatePresence>;
 };
 
 export default Home;
