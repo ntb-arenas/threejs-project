0f3b17149adc3d111d9d28bea6ddf88dc6b22bae
Niño Arenas <nino.arenas@wundermanthompson.com>
2023-12-14 18:03:31 +0000

    Update camera position

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -1,5 +1,25 @@
-const CameraRig = () => {
-  return <div>CameraRig</div>;
+import { useRef } from "react";
+
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
+import { useSnapshot } from "valtio";
+
+import state from "../store";
+
+const CameraRig = ({ children }) => {
+  const group = useRef();
+  const snap = useSnapshot(state);
+
+  useFrame((state, delta) => {
+    easing.dampE(
+      group.current.rotation,
+      [state.pointer.y / 10, -state.pointer.x / 5, 0],
+      0.25,
+      delta
+    );
+  });
+
+  return <group ref={group}>{children}</group>;
 };
 
 export default CameraRig;
--- client/src/canvas/index.jsx
+++ client/src/canvas/index.jsx
@@ -11,13 +11,13 @@ const CanvasModel = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="city" />
 
-      {/* <CameraRig> */}
-      {/* <Backdrop> */}
-      <Center>
-        <Shirt />
-      </Center>
-      {/* </Backdrop> */}
-      {/* </CameraRig> */}
+      <CameraRig>
+        {/* <Backdrop> */}
+        <Center>
+          <Shirt />
+        </Center>
+        {/* </Backdrop> */}
+      </CameraRig>
     </Canvas>
   );
 };
